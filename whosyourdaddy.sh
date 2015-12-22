6155435d79f5acb4d488be41e289009e7592a833
Xin Zhang <zju.zhangxin@gmail.com>
2015-12-22 15:27:22 +0800

    update

---
+++ whosyourdaddy.sh
@@ -0,0 +1,14 @@
+buf_size=1000000 xelatex main.tex
+buf_size=1000000 xelatex main.tex
+bibtex main
+buf_size=1000000 xelatex main.tex
+buf_size=1000000 xelatex main.tex
+
+
+rm *.aux
+rm *.bbl
+rm *.blg
+rm *.idx
+rm *.log
+rm *.out
+rm *.toc
