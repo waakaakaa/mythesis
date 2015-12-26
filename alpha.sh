edc86ad4aa2f46265410d2a53b1969f310e5fff8
Xin Zhang <zju.zhangxin@gmail.com>
2015-12-26 16:26:36 +0800

    complete raw version

---
+++ alpha.sh
@@ -0,0 +1,2 @@
+buf_size=1000000 xelatex main.tex
+buf_size=1000000 xelatex main.tex
---
+++ gamma.sh
@@ -0,0 +1,8 @@
+rm main.aux
+rm main.bbl
+rm main.blg
+rm main.log
+rm main.idx
+rm main.out
+rm main.toc
+open main.pdf
